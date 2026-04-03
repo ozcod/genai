1463831f1aa73a71e2c282b65e11142d3d55f84e
Ozair Ahmad <ozair.ahmad@stud.uni-bamberg.de>
2026-04-03 10:35:51 +0200

    feat: Add chatbot with web seach

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import readline from "node:readline/promises";
 import Groq from "groq-sdk";
 import { tavily } from "@tavily/core";
 
@@ -9,72 +10,95 @@ const groq = new Groq({
 });
 
 async function main() {
-  const messages = [
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const message = [
     {
       role: "system",
       content: `You are a smart assistant that helps users with their questions.
           You have access to the following tools:
           1. webSearch({query}:{query:string}) // Search the web for the given query and return the results from internet.
-          Always use the tool when you don't know the answer to a question.`,
-    },
-    {
-      role: "user",
-      content: "When was iphone 17 released?",
+          Always use the tool when you don't know the answer to a question.
+          current datetime: ${new Date().toUTCString()}`,
     },
   ];
-  const completions = await groq.chat.completions.create({
-    model: "openai/gpt-oss-20b",
-    temperature: 0,
-    messages: messages,
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description:
-            "Search the web for the given query and return the results from internet.",
-          parameters: {
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform search on",
+
+  while (true) {
+    const question = await rl.question("User: ");
+    if (question === "exit") {
+      break;
+    }
+    message.push({
+      role: "user",
+      content: question,
+    });
+
+    while (true) {
+      const completions = await groq.chat.completions.create({
+        model: "openai/gpt-oss-20b",
+        temperature: 0,
+        messages: message,
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "webSearch",
+              description:
+                "Search the web for the given query and return the results from internet.",
+              parameters: {
+                type: "object",
+                properties: {
+                  query: {
+                    type: "string",
+                    description: "The search query to perform search on",
+                  },
+                },
+                required: ["query"],
               },
             },
-            required: ["query"],
           },
-        },
-      },
-    ],
-    tool_choice: "auto",
-  });
+        ],
+        tool_choice: "auto",
+      });
 
-  const tool_calls = completions.choices[0].message.tool_calls;
-  if (!tool_calls) {
-    // console.log(`Assistant: ${completions.choices[0].message.content}`);
-    return;
-  }
+      message.push(completions.choices[0].message);
 
-  for (const tool of tool_calls) {
-    const functionName = tool.function.name;
-    const functionArgs = tool.function.arguments;
+      const tool_calls = completions.choices[0].message.tool_calls;
 
-    if (functionName === "webSearch") {
-      const toolResult = await webSearch(JSON.parse(functionArgs));
-      //   console.log("toolResult", toolResult);
+      if (!tool_calls) {
+        console.log(`Assistant: ${completions.choices[0].message.content}`);
+        break;
+      }
+
+      for (const tool of tool_calls) {
+        const functionName = tool.function.name;
+        const functionArgs = tool.function.arguments;
+
+        if (functionName === "webSearch") {
+          const toolResult = await webSearch(JSON.parse(functionArgs));
+          //   console.log("toolResult", toolResult);
+          message.push({
+            role: "tool",
+            name: functionName,
+            content: toolResult,
+            tool_call_id: tool.id,
+          });
+        }
+      }
     }
   }
-
-  console.log(JSON.stringify(completions.choices[0].message, null, 2));
+  rl.close();
 }
 main();
 
 async function webSearch({ query }) {
-  //   console.log(`Calling tool function ${query}`);
+  console.log(`Calling web search function ${query}`);
   const response = await tvly.search(query);
   const finalResponse = response.results
     .map((result) => result.content)
     .join("\n\n");
-  console.log("response", finalResponse);
+  // console.log("response", finalResponse);
   return finalResponse;
 }
